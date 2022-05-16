659b27009c6b17ea160cdc77543106a2dffb5d5a
Luuk05 <74959722+Luuk05@users.noreply.github.com>
2022-05-16 19:13:16 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import "./App.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import styled, { createGlobalStyle } from "styled-components";
+import FormControl from "@mui/material/FormControl";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+
+// function
+
+const theme = createTheme({
+  typography: {
+    allVariants: {
+      textTransform: "capitalize",
+      fontSize: 16,
+    },
+  },
+});
+
+let App = () => {
+  let [value, setValue] = useState();
+
+  return (
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <ThemeProvider theme={theme}>
+        <h1>Bedrag</h1>
+        <FormControl sx={{ m: 1 }}>
+          <InputLabel htmlFor="amount">Amount</InputLabel>
+          <OutlinedInput
+            id="amount"
+            type="number"
+            startAdornment={<InputAdornment position="start">€</InputAdornment>}
+            label="Amount"
+            onChange={(e) => setValue(e.target.value)}
+          />
+          <Button variant="contained" disabled={!value}>
+            Voeg toe
+          </Button>
+        </FormControl>
+        <h1>Categorie</h1>
+      </ThemeProvider>
+    </Grid>
+  );
+};
+
+export default App;
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyClTbOwwZFXxLFWa-omegQs0uHyGOsoUto",
+  authDomain: "sqlproject-e581a.firebaseapp.com",
+  projectId: "sqlproject-e581a",
+  storageBucket: "sqlproject-e581a.appspot.com",
+  messagingSenderId: "715687704751",
+  appId: "1:715687704751:web:29a4e1621cf2e11369fc5a",
+};
+
+const app = initializeApp(firebaseConfig);

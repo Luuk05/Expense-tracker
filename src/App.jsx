b2b3e37293f0a7017f6425c9385f09b3646c52df
Luuk05 <74959722+Luuk05@users.noreply.github.com>
2022-05-16 20:04:41 +0200

    Readme change test

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,8 @@ import InputAdornment from "@mui/material/InputAdornment";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 
-// function
+
+
 
 const theme = createTheme({
   typography: {
@@ -20,35 +21,46 @@ const theme = createTheme({
   },
 });
 
+
+
+
+// let App = () => {
+//   let [value, setValue] = useState();
+
+//   return (
+//     <Grid
+//       container
+//       direction="column"
+//       alignItems="center"
+//       justifyContent="center"
+//     >
+//       <ThemeProvider theme={theme}>
+//         <h1>Bedrag</h1>
+//         <FormControl sx={{ m: 1 }}>
+//           <InputLabel htmlFor="amount">Amount</InputLabel>
+//           <OutlinedInput
+//             id="amount"
+//             type="number"
+//             startAdornment={<InputAdornment position="start">€</InputAdornment>}
+//             label="Amount"
+//             onChange={(e) => setValue(e.target.value)}
+//           />
+//           <Button variant="contained" disabled={!value}>
+//             Voeg toe
+//           </Button>
+//         </FormControl>
+//         <h1>Categorie</h1>
+//       </ThemeProvider>
+//     </Grid>
+//   );
+// };
+
 let App = () => {
-  let [value, setValue] = useState();
+  let [users, setUsers] = useState([]);
 
   return (
-    <Grid
-      container
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-    >
-      <ThemeProvider theme={theme}>
-        <h1>Bedrag</h1>
-        <FormControl sx={{ m: 1 }}>
-          <InputLabel htmlFor="amount">Amount</InputLabel>
-          <OutlinedInput
-            id="amount"
-            type="number"
-            startAdornment={<InputAdornment position="start">€</InputAdornment>}
-            label="Amount"
-            onChange={(e) => setValue(e.target.value)}
-          />
-          <Button variant="contained" disabled={!value}>
-            Voeg toe
-          </Button>
-        </FormControl>
-        <h1>Categorie</h1>
-      </ThemeProvider>
-    </Grid>
+    <h1> hello </h1>
   );
-};
+}
 
 export default App;

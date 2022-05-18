0d5da32e467a51121060fa5b635171232949c118
Luuk05 <74959722+Luuk05@users.noreply.github.com>
2022-05-18 13:38:51 +0200

    style commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,30 @@
 import React, { useEffect, useState } from "react";
-import {db} from "./firebaseConfig"
-import { collection, getDocs, addDoc } from "firebase/firestore"
-import { ThemeProvider } from "@mui/material/styles";
-import { theme } from "./components/theme.js"
+import { db } from "./firebaseConfig";
+import { collection, getDocs, addDoc } from "firebase/firestore";
 import FormControl from "@mui/material/FormControl";
 import OutlinedInput from "@mui/material/OutlinedInput";
-import TextField from '@mui/material/TextField';
+import TextField from "@mui/material/TextField";
 import InputLabel from "@mui/material/InputLabel";
 import InputAdornment from "@mui/material/InputAdornment";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
-
-
-
-
+import Select from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 
 let App = () => {
-
-
   return (
     <Grid
       container
       direction="column"
       alignItems="center"
       justifyContent="center"
-    >
-      <ThemeProvider theme={theme}>
-      
-      </ThemeProvider>
-    </Grid>
+    ></Grid>
   );
 };
-
 
 export default App;

855accf011121f5fecf1864b2211e1a0ca658efe
Luuk05 <74959722+Luuk05@users.noreply.github.com>
2022-06-01 17:56:38 +0200

    werkende app zonder routing nog

--- src/App.jsx
+++ src/App.jsx
@@ -4,44 +4,39 @@ import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Home from "./components/Home";
+import Login from "./components/Login";
 
 let App = () => {
-    let [userSigendIn, setUserSignedIn] = useState(false);
+  let [userSigendIn, setUserSignedIn] = useState(false);
 
-    const provider = new GoogleAuthProvider();
+  let provider = new GoogleAuthProvider();
+  provider.setCustomParameters({
+    prompt: "select_account",
+  });
 
-    let signInWithGoogle = () => {
-        signInWithPopup(auth, provider).then((result) => {
-            setUserSignedIn(true);
-        }).catch((error) => {
-            console.log(error);
-        });
-    }
+  let signInWithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        setUserSignedIn(true);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-    if (userSigendIn) {
-        return (
-            <>
-                <Home setUserSignedIn={setUserSignedIn} />
-            </>
-            
-        )
-    } else {
-        return (
-            <Grid
-                container
-                direction="column"
-                alignItems="center"
-                justifyContent="center"
-                padding={4}
-            >
-               <Button variant="contained" onClick={signInWithGoogle}>Sign in with Google</Button>
-            </Grid>
-            );
-    }
-
-    
-
-
-}
+  if (userSigendIn) {
+    return (
+      <>
+        <Home setUserSignedIn={setUserSignedIn} />
+      </>
+    );
+  } else {
+    return (
+      <>
+        <Login signInWithGoogle={signInWithGoogle} />
+      </>
+    );
+  }
+};
 
 export default App;

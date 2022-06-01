d88d9f4be6b2f1ccc93f4a033097dff3f8ea9f35
Luuk05 <74959722+Luuk05@users.noreply.github.com>
2022-06-01 18:22:54 +0200

    werkende app met routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,11 @@ import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
 import Home from "./components/Home";
 import Login from "./components/Login";
+import { Routes, Route, Link, Navigate, useNavigate } from "react-router-dom";
 
 let App = () => {
   let [userSigendIn, setUserSignedIn] = useState(false);
+  const navigate = useNavigate();
 
   let provider = new GoogleAuthProvider();
   provider.setCustomParameters({
@@ -18,25 +20,41 @@ let App = () => {
     signInWithPopup(auth, provider)
       .then((result) => {
         setUserSignedIn(true);
+        navigate("/");
       })
       .catch((error) => {
         console.log(error);
       });
   };
 
-  if (userSigendIn) {
-    return (
-      <>
-        <Home setUserSignedIn={setUserSignedIn} />
-      </>
-    );
-  } else {
-    return (
-      <>
-        <Login signInWithGoogle={signInWithGoogle} />
-      </>
-    );
-  }
+  return (
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Home setUserSignedIn={setUserSignedIn} userSigendIn={userSigendIn} />
+        }
+      />
+      <Route
+        path="/login"
+        element={<Login signInWithGoogle={signInWithGoogle} />}
+      />
+    </Routes>
+  );
+
+  // if (userSigendIn) {
+  //   return (
+  //     <>
+  //       <Home setUserSignedIn={setUserSignedIn} />
+  //     </>
+  //   );
+  // } else {
+  //   return (
+  //     <>
+  //       <Login signInWithGoogle={signInWithGoogle} />
+  //     </>
+  //   );
+  // }
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@mui/material/styles";
 import { createTheme } from "@mui/material/styles";
 import "./index.css";
 import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 
 const theme = createTheme({
   typography: {
@@ -21,6 +22,8 @@ const theme = createTheme({
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <ThemeProvider theme={theme}>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </ThemeProvider>
 );
